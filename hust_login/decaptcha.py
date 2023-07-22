69ff0e17d8c10ebfa0f64243ba3ac00deb0026cc
Jackhr <jackhr159@qq.com>
2023-07-22 13:14:54 +0800

    using logging, provide solution

--- hust_login/decaptcha.py
+++ hust_login/decaptcha.py
@@ -1,9 +1,10 @@
 from io import BytesIO
 import pytesseract
+from logging import root as log
 from PIL import Image
 
 def decaptcha(img_content):
-    print('decaptching...')
+    log.debug('decaptching...')
     img_list = []
     with Image.open(BytesIO(img_content)) as img_gif:
         for i in range(img_gif.n_frames):
@@ -14,7 +15,11 @@ def decaptcha(img_content):
     for pos in [(x,y) for x in range(width) for y in range(height)]:
         if sum([img.getpixel(pos) < 254 for img in img_list]) >= 3:
             img_merge.putpixel(pos,0)
-    captcha_code = pytesseract.image_to_string(img_merge, config='-c tessedit_char_whitelist=0123456789 --psm 6')
-    print('captcha_code:{}'.format(captcha_code.strip()))
+    try:
+        captcha_code = pytesseract.image_to_string(img_merge, config='-c tessedit_char_whitelist=0123456789 --psm 6')
+    except pytesseract.TesseractNotFoundError:
+        log.fatal('tesseract is not installed !!', exc_info=True)
+        raise EnvironmentError('USE sudo apt install tesseract-ocr OR go to https://tesseract-ocr.github.io/tessdoc/Downloads.html')
+    log.debug('captcha_code:{}'.format(captcha_code.strip()))
     # img_merge.save('./src/decaptcha/blended.png')
     return captcha_code
