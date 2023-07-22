ce4baf3f13fcc1e0223dcb70ebf274ba56459241
MarvinTerry <marvinterry2004@gmail.com>
2023-07-22 20:48:51 +0800

    增加自动计算周次功能
    去除GetOneDay函数的week参数。
    从html爬取学期开始日期，并添加自动计算周次功能。
    增加保障日期格式正确的机制。
    修改测试文件。

--- hust_login/curriculum.py
+++ hust_login/curriculum.py
@@ -2,25 +2,41 @@
 import requests
 import json
 from .login import CheckLoginStatu
+import re
+from datetime import datetime
 
-def GetOneDay(session:requests.Session, day:str, week:str) -> list[dict]:
+def weeks_from(start_date, date):
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    date = datetime.strptime(date, '%Y-%m-%d')
+    delta = date - start_date
+    return delta.days // 7 + 1
+
+def GetOneDay(session:requests.Session, date_query:str) -> list[dict]:
     '''
     PARAMETERS:\n
     session -- should be already logged in\n
-    day     -- the day you want, in form of 2023-04-01\n
-    week    -- the weeks since term started， in form of 1\n
+    date     -- the day you want, in form of YYYY-MM-DD\n
     \n
     RETURN:\n
     [{'No':'1', 'ClassName': 'XXX', 'TeacherName': 'XXX'}}]
     '''
-    if not (isinstance(session, requests.Session) and isinstance(day, str) and isinstance(week, str)):
+    if not (isinstance(session, requests.Session) and isinstance(date_query, str)):
         raise TypeError('HUSTPASS: CHECK YOUR session, day AND week INPUT TYPE')
     
     if not CheckLoginStatu(session):
         raise ConnectionRefusedError('HUSTPASS: YOU HAVENT LOGGED IN')
     
-    resp = session.get('http://hub.m.hust.edu.cn/kcb/todate/JsonCourse.action?sj={}&zc={}'.format(day, week))
-    content=json.loads(resp.text)
+    date_query = datetime.strptime(date_query, '%Y-%m-%d').date().isoformat() # 保证日期格式标准，即补充用户可能忘记添加的0
+    
+    resp_html = session.get('http://hub.m.hust.edu.cn/kcb/todate/datecourseNew.action')
+
+    start_date_semester = re.search(
+        'var sDate = "(.*)";', resp_html.text).group(1) # 从html抓取学期开始日期
+    
+    week = weeks_from(start_date_semester, date_query)
+    
+    resp_api = session.get('http://hub.m.hust.edu.cn/kcb/todate/JsonCourse.action?sj={}&zc={}'.format(date_query, week))
+    content=json.loads(resp_api.text)
     ret = []
     for item in content:
         if item['kc'][0]['JSMC']=='—':
