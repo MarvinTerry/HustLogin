672290597b5414569bbf583b44e782da937711c7
Jackhr <jackhr159@qq.com>
2023-07-22 13:13:36 +0800

    using logging, input

--- test_basic.py
+++ test_basic.py
@@ -1,6 +1,11 @@
 from hust_login.login import HustPass
+import logging
+logging.basicConfig(level=logging.DEBUG,\
+                    format='[%(levelname)s]  %(message)s')
 
+Uname = input('Uid:')
+Upass = input('Pwd:')
 
-with HustPass('', '') as s:
+with HustPass(Uname, Upass) as s:
     resp = s.get('http://m.hust.edu.cn/wechat/apps_center.jsp')
     print(resp.text)
