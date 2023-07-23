51d3a7f95bec96101406ea3dd27aa43a53d8d719
Jackhr <jackhr159@qq.com>
2023-07-23 15:31:20 +0800

    add multi input support

--- hust_login/utility_bills.py
+++ hust_login/utility_bills.py
@@ -1,37 +1,78 @@
 import requests
 import re
 from .login import CheckLoginStatu
+from datetime import datetime
 
-def GetElectricityBill(session:requests.Session, Uid:str, QueryDates:list) -> dict:
+def DateLoad(_QueryDate:str) -> str:
+    if re.search('(\d*/\d*/\d*)', _QueryDate) is None :
+        if re.search('(\d*-\d*-\d*)', _QueryDate) is None:
+            raise
+        _date = datetime.strptime(_QueryDate, '%Y-%m-%d')
+    else:
+        _date = datetime.strptime(_QueryDate, '%Y/%m/%d')
+    return _date
+
+def DateFormat(_date:datetime):
+    date = _date.date().timetuple()
+    return str(date.tm_year)+'/'+str(date.tm_mon)+'/'+str(date.tm_mday)
+
+def GetElectricityBill(session:requests.Session, Uid:str, _QueryDate:str|list[str]|tuple[str,str]) -> dict:
+    '''
+    PARAMETERS:\n
+    session     -- should be already logged in\n
+    Uid         -- str  : your student uid\n
+    QueryDates  -- str  : in form of '2023-7-21' or '2023/7/21'\n
+                -- list : a list, each item in the same form\n
+                -- tuple: two str, including the start and the end\n
+    \n
+    RETURN:\n
+    {'RoomName': 'XXX', 'RemainPower': 'XXX', 'DayCost': [{'daycost': 'XXX', 'date': 'YYYY-MM-DD', 'money': 'XXX'}]}
+    '''
+    session.get('http://pass.hust.edu.cn/cas/login?service=http://sdhq.hust.edu.cn/icbs/hust/cas/neusoftcas.aspx')
+    Query_list = []
+    if isinstance(_QueryDate, list):
+        for _item in _QueryDate:
+            Query_list.append(DateFormat(DateLoad(_item)))
+
+    elif isinstance(_QueryDate, tuple):
+        if len(_QueryDate) !=2:
+            raise ValueError('HUSTPASS: ONLY ("XXX","XXX") LIKE TUPLE IS ACCEPTED')
+        _start_date, _end_date = _QueryDate
+        S_date = DateLoad(_start_date)
+        E_date = DateLoad(_end_date)
+        if S_date == E_date:
+            Query_list.append(DateFormat(S_date))
+        elif S_date > E_date:
+            S_date = DateLoad(_end_date)
+            E_date = DateLoad(_start_date)
+
+        if len(Query_list)!=1:
+            Query_list = (DateFormat(S_date),DateFormat(E_date))
+
+    elif isinstance(_QueryDate, str):
+        Query_list.append(DateFormat(DateLoad(_QueryDate)))
+
+    else:
+        raise TypeError('HUSTPASS: UNSUPPORT TYPE')
+
     if not CheckLoginStatu(session):
         raise ConnectionRefusedError('HUSTPASS: YOU HAVENT LOGGED IN')
-    try:
-        _startDate = QueryDates[0]
-        _endDate = QueryDates[1]
-    except:
-        raise
-    if re.search('(\d*/\d*/\d*)', _startDate) is None or re.search('(\d*/\d*/\d*)', _endDate) is None:
-        raise
 
-    #jar = requests.cookies.RequestsCookieJar()
-    #for cookie in cookies.split(";"):
-    #    key, value = cookie.split("=", 1)
-    #    jar.set(key, value)
-
-    resp0 = session.get('http://sdhq.hust.edu.cn/icbs/PurchaseWebService.asmx/getRoomInfobyStudentID?Student_ID={}'.format(Uid))
-    payload0 = resp0.text
+    payload0 = session.get(
+        'http://sdhq.hust.edu.cn/icbs/PurchaseWebService.asmx/getRoomInfobyStudentID?Student_ID={}'.format(Uid)).text
     if re.search('<msg>成功</msg>', payload0) is None:
-        print(resp0)
         raise
-    _RNo = re.search('<RoomNo>(.*</RoomNo>', payload0).group(1)
+    _RNo = re.search('<RoomNo>(.*)</RoomNo>', payload0).group(1)
     ret_Name = re.search('<RoomName>(.*)</RoomName>', payload0).group(1)
 
-    payload1 = session.get('http://sdhq.hust.edu.cn/icbs/PurchaseWebService.asmx/getMeterInfo?Room_ID={}'.format(_RNo)).text
+    payload1 = session.get(
+        'http://sdhq.hust.edu.cn/icbs/PurchaseWebService.asmx/getMeterInfo?Room_ID={}'.format(_RNo)).text
     if re.search('<msg>成功</msg>', payload1) is None:
         raise
     MeterID = re.search('<meterId>(.*)</meterId>', payload1).group(1)
 
-    payload2 = session.get('http://sdhq.hust.edu.cn/icbs/PurchaseWebService.asmx/getReserveHKAM?AmMeter_ID={}'.format(MeterID)).text
+    payload2 = session.get(
+        'http://sdhq.hust.edu.cn/icbs/PurchaseWebService.asmx/getReserveHKAM?AmMeter_ID={}'.format(MeterID)).text
     if re.search('<msg>成功</msg>', payload2) is None:
         raise
     _remainPower = re.search('<remainPower>(.*)</remainPower>', payload2).group(1)
@@ -39,7 +80,18 @@ def GetElectricityBill(session:requests.Session, Uid:str, QueryDates:list) -> di
     price_per_unit = re.search('<basePrice>(.*)</basePrice>',payload2).group(1)
     ret_remainPower = _remainPower+_unit
     
-    payload3 = session.get('http://sdhq.hust.edu.cn/icbs/PurchaseWebService.asmx/getMeterDayValue?AmMeter_ID={}&startDate=2023/7/21&endDate=2023/7/21'.format(MeterID)).text
+    ret = []
+    if isinstance(Query_list, list):
+        for item in Query_list:
+            ret.append(_GetOneDay(session, MeterID, item, item)[0])
+    elif isinstance(Query_list, tuple):
+        ret = _GetOneDay(session, MeterID, Query_list[0], Query_list[1])
+
+    return {'RoomName':ret_Name, 'RemainPower':ret_remainPower, 'DayCosts':ret}
+
+def _GetOneDay(session:requests.Session, MeterID:str, S_Date:str, E_Date:str) -> list[dict]:
+    payload3 = session.get(
+        'http://sdhq.hust.edu.cn/icbs/PurchaseWebService.asmx/getMeterDayValue?AmMeter_ID={}&startDate={}&endDate={}'.format(MeterID, S_Date, E_Date)).text
     if re.search('<msg>成功</msg>', payload3) is None:
         raise
     ret_daycost = []
@@ -50,5 +102,4 @@ def GetElectricityBill(session:requests.Session, Uid:str, QueryDates:list) -> di
         money = re.search('<dayUseMeony>(.*)</dayUseMeony>',item).group(1)
         date = re.search('<curDayTime>(.*)</curDayTime>', item).group(1)
         ret_daycost.append({'daycost':elc_cost+cost_unit,'date':date,'money':money})
-    
-    return {'RoomName':ret_Name, 'RemainPower':ret_remainPower, 'DayCost':ret_daycost}
+    return ret_daycost
