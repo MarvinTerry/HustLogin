253693f6033c6a9a46b94bc5c3994ef6c22edfeb
MarvinTerry <marvinterry2004@gmail.com>
2023-07-27 13:50:54 +0800

    UPDATE API FORMAT
    统整api返回数据中键值的名称

--- test_basic.py
+++ test_basic.py
@@ -13,8 +13,13 @@ with open('secret.txt', 'r') as f:
 
 
 with HustPass(Uname, Upass) as s:
-    with open('res.json','w', encoding='utf-8') as f:
-        f.write(json.dumps(s.QueryCurriculum(
-            ('2023-04-03','2023-04-10')), ensure_ascii=False))
-    
+    with open('curriculum.json','w', encoding='utf-8') as f:
+        f.write(json.dumps(s.QueryCurriculum(('2023-04-03','2023-04-10')), ensure_ascii=False))
+    with open('ecard_bill.json','w', encoding='utf-8') as f:
+        f.write(json.dumps(s.QueryEcardBills('2023-04'), ensure_ascii=False))
+    with open('free_room.json','w', encoding='utf-8') as f:
+        f.write(json.dumps(s.QueryFreeRoom('2023-04-03'), ensure_ascii=False))
+    with open('electricity_bill.json','w', encoding='utf-8') as f:
+        f.write(json.dumps(s.QueryElectricityBills(('2023-04-03','2023-04-10')), ensure_ascii=False))
+        
     
