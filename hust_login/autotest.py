e1119cd24ca7f92aa39b78bf7db9b0efc95614d4
Jackhr <jackhr159@qq.com>
2023-07-28 20:33:35 +0800

    add cli fast action

--- hust_login/autotest.py
+++ hust_login/autotest.py
@@ -1,7 +1,4 @@
-from . import HustPass
-
-def full_test(Uid,Pwd):
-    hpass = HustPass(Uid, Pwd)
+def full_test(hpass):
     try:
         hpass.QueryCurriculum('2023-04-27')
         hpass.QueryCurriculum(['2023-04-27','2023-03-27','2023-04-07'])
