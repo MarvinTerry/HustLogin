4eec3ad10f83d70199bd74428c40a54cd8fe5019
MarvinTerry <marvinterry2004@gmail.com>
2023-08-19 19:24:41 +0800

    修复课表学期问题
    增加学期参数选项

--- test_basic.py
+++ test_basic.py
@@ -14,12 +14,12 @@ with open('secret.txt', 'r') as f:
 
 with HustPass(Uname, Upass) as s:
     with open('curriculum.json','w', encoding='utf-8') as f:
-        f.write(json.dumps(s.QuerySchedules(('2023-04-03','2023-04-10')), ensure_ascii=False))
-    with open('ecard_bill.json','w', encoding='utf-8') as f:
-        f.write(json.dumps(s.QueryEcardBills('2023-04'), ensure_ascii=False))
-    with open('free_room.json','w', encoding='utf-8') as f:
-        f.write(json.dumps(s.QueryFreeRooms('2023-04-03'), ensure_ascii=False))
-    with open('electricity_bill.json','w', encoding='utf-8') as f:
-        f.write(json.dumps(s.QueryElectricityBills(('2023-04-03','2023-04-10')), ensure_ascii=False))
+        f.write(json.dumps(s.QuerySchedules(('2023-08-28','2023-09-01'),semester='20231'), ensure_ascii=False))
+    # with open('ecard_bill.json','w', encoding='utf-8') as f:
+    #     f.write(json.dumps(s.QueryEcardBills('2023-04'), ensure_ascii=False))
+    # with open('free_room.json','w', encoding='utf-8') as f:
+    #     f.write(json.dumps(s.QueryFreeRooms('2023-04-03'), ensure_ascii=False))
+    # with open('electricity_bill.json','w', encoding='utf-8') as f:
+    #     f.write(json.dumps(s.QueryElectricityBills(('2023-04-03','2023-04-10')), ensure_ascii=False))
         
     
