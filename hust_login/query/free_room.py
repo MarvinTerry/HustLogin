f27fd747a6fa091ff4535e842f447d507bed1a4e
Jackhr-arch <63526062+Jackhr-arch@users.noreply.github.com>
2023-09-16 00:41:41 +0800

    code: reduce output

--- hust_login/query/free_room.py
+++ hust_login/query/free_room.py
@@ -9,7 +9,7 @@ def GetFreeRooms(session:requests.Session, _date_query:str) -> dict:
     date    -- str  : the day you want, in form of YYYY-MM-DD\n
     \n
     RETURN:\n
-    {'Date':'YYYY-MM-DD','Buildings':['东九楼A':{'No':'1','Roomlist': ['A101','A102']}]}
+    {'Date':'YYYY-MM-DD','Buildings':{'东九楼A':{'No':'1','RoomList': ['A101','A102']}}}
     '''
     if isinstance(_date_query, str):
         date_query = DateFormat(_date_query)
@@ -37,7 +37,7 @@ def _GetOneDay(session:requests.Session, date_query:str) -> list:
 
     raw_data = []
     # 建立数据结构
-    ret = {'Date':date_query,'Buildings':{buiding_name: [{'No': str(i), 'Roomlist': []} for i in range(1,13)] for buiding_name in __buildings.keys()}}
+    ret = {'Date':date_query,'Buildings':{buiding_name: [{'No': str(i), 'RoomList': []} for i in range(1,13)] for buiding_name in __buildings.keys()}}
 
     for buiding_id in __buildings.values():
         # 爬取每个教学楼数据
@@ -46,5 +46,14 @@ def _GetOneDay(session:requests.Session, date_query:str) -> list:
     
     for item in raw_data:
         ret['Buildings'][item['JXLMC']][item['JC']-1]['Roomlist'].append(item['JSMC'].strip('教室'))
+    
+    # Del empty ones
+    for name in list(ret['Buildings'].keys()):
+        _item = ret['Buildings'][name]
+        for item in reversed(_item):
+            if len(item['RoomList']) == 0:
+                _item.remove(item)
+        if len(_item) == 0:
+            del ret['Buildings'][name]
 
     return ret
